c323f14ec825e06bb5f1648134355b52922a82b5
Anjany Pandey <anjany.pandey06@gmail.com>
2025-01-14 19:45:52 +0530

    UI: Completed About page

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -58,20 +58,12 @@ const About = () => {
           </div>
         </div>
       </div>
+      {/* SECOND CONTAINER */}
       <div className="flex items-center flex-col lg:flex-row gap-8 mt-36">
-        {/* IMAGE - LEFT SIDE */}
-        <div className="flex-1">
-          <div className="relative">
-            <img src={about1} alt="" className="rounded-3xl" />
-            <span className="absolute top-8 left-8 bg-white px-2 rounded-full medium-14">
-              San Francisco
-            </span>
-          </div>
-        </div>
-        {/* INFO RIGHT SIDE */}
+        {/* INFO LEFT SIDE */}
         <div className="flex-1">
           <h2 className="h2">
-            Empowering You to Find Your Dream Home, Effortlessly
+            Simplifying Your Real Estate Journey Every Step of the Way
           </h2>
           <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid
@@ -80,25 +72,34 @@ const About = () => {
           </p>
           <div className="flex flex-col gap-6 mt-5">
             <div className="flex gap-3">
-              <FaScreenpal className="text-secondary" />
-              <p>Virtual property tours and viewings</p>
+              <FaList className="text-secondary" />
+              <p>In-app scheduling for property viewing</p>
             </div>
             <div className="flex gap-3">
               <FaUpDown className="text-secondary" />
               <p>Real-time market price updates</p>
             </div>
             <div className="flex gap-3">
-              <FaMap className="text-secondary" />
-              <p>Interactive floor plans and maps</p>
+              <FaInbox className="text-secondary" />
+              <p>User friendly interfaces for smooth navigation</p>
+            </div>
+            <div className="flex gap-3">
+              <FaUser className="text-secondary" />
+              <p>Detailed agent and realtor profiles</p>
             </div>
             <div className="flex gap-3">
               <FaMapMarkedAlt className="text-secondary" />
               <p>Access to off-market properties</p>
             </div>
-            <div className="flex gap-3">
-              <FaEnvelope className="text-secondary" />
-              <p>Direct messaging with agents and owwners</p>
-            </div>
+          </div>
+        </div>
+        {/* IMAGE - RIGHT SIDE */}
+        <div className="flex-1">
+          <div className="relative flex justify-end">
+            <img src={about2} alt="" className="rounded-3xl" />
+            <span className="absolute top-8 right-8 bg-white px-2 rounded-full medium-14">
+              GOLDEN COAST
+            </span>
           </div>
         </div>
       </div>
