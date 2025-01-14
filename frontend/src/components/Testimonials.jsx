4a9881097596a8fd5412c5e9f95c935fbfe57329
Anjany Pandey <anjany.pandey06@gmail.com>
2025-01-14 20:36:01 +0530

    UI: tettinomials component structure done

--- frontend/src/components/Testimonials.jsx
+++ frontend/src/components/Testimonials.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import React from "react";
+import user1 from "../assets/testimonials/user1.png";
+import user2 from "../assets/testimonials/user2.png";
+
+import { FaStar, FaCheck } from "react-icons/fa6";
+
+import property1 from "../assets/img1.png";
+import property2 from "../assets/img2.png";
+import property3 from "../assets/img3.png";
+import property4 from "../assets/img4.png";
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
-  )
-}
+    <section className="max-padd-container bg-white py-16 xl:pt-28">
+      {/* CONTAINER */}
+      <div className="">
+        {/* LEFT SIDE */}
+        <div>
+          <h2>Customer feedback and experiences</h2>
+          <div>
+            <div>
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <FaStar />
+            </div>
+            <div>
+              Showing 1-2 out of <b>2k reviews</b>
+            </div>
+          </div>
+        </div>
+        {/* RIGHT - SIDE */}
+        <div>
+          {/* TESTIMONIAL CARD */}
+          <div>
+            <div>
+              <div>
+                <img
+                  src={user1}
+                  alt="userImg"
+                  height={44}
+                  width={44}
+                  className="rounded-full"
+                />
+                <h5>John Doe</h5>
+              </div>
+              <div>
+                <FaCheck />
+                Verified
+              </div>
+            </div>
+            <hr />
+            <div>
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <FaStar />
+            </div>
+            <h4>High Quality</h4>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio
+              harum consequuntur tenetur ipsam vel quam aperiam repudiandae odit
+              distinctio error?Lorem ipsum dolor, sit amet consectetur
+              adipisicing.
+            </p>
+            <div>
+              <img
+                src={property1}
+                alt=""
+                height={44}
+                width={44}
+                className="rounded aspect-square object-cover"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Testimonials
+export default Testimonials;
