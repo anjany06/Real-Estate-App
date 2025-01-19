e1380c7a870b818c621af9d5e5227eaec9a9b2af
Anjany Pandey <anjany.pandey06@gmail.com>
2025-01-19 22:03:04 +0530

    `Update GeoCodeMarker component to use react-leaflet and add default icon`

--- frontend/src/components/GeoCodeMarker.jsx
+++ frontend/src/components/GeoCodeMarker.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import { Marker } from 'react-leaflet'
+import React, { useState } from "react";
+import { DefaultContext } from "react-icons";
+import { Marker, Popup, useMap } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
+L.Marker.prototype.options.icon = DefaultIcon;
 
 const GeoCodeMarker = () => {
+  const map = useMap();
+  const [positon, setPosition] = useState([60, 19]);
   return (
-    <Marker positon= {positon}></Marker>
-  )
-}
+    <Marker positon={positon} icon={DefaultIcon}>
+      <Popup />
+    </Marker>
+  );
+};
 
-export default GeoCodeMarker
+export default GeoCodeMarker;
