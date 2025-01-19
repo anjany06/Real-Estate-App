22f7f87f10bce00efbceb348b343f11dd033803a
Anjany Pandey <anjany.pandey06@gmail.com>
2025-01-20 00:00:16 +0530

    `Added esri-leaflet-geocoder and related dependencies for geocoding functionality`

--- frontend/src/components/GeoCodeMarker.jsx
+++ frontend/src/components/GeoCodeMarker.jsx
@@ -4,6 +4,7 @@ import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import icon from "leaflet/dist/images/marker-icon.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import * as ELG from "esri-leaflet-geocoder";
 
 let DefaultIcon = L.icon({
   iconUrl: icon,
@@ -36,6 +37,17 @@ const GeoCodeMarker = ({ address }) => {
     return null;
   }
 
+  useEffect(() => {
+    ELG.geocode()
+      .text(address)
+      .run((err, results, response) => {
+        if (results?.results?.length > 0) {
+          const { lat, lng } = results?.results[0].latlng;
+          setPosition([lat, lng]);
+          map.flyTo([lat, lng], 6);
+        }
+      });
+  }, [address]);
   return (
     <Marker position={position} icon={DefaultIcon}>
       <Popup />
