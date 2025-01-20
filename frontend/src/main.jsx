aa4803ba0f1594e9a973802a766af48c7bf6de01
Anjany Pandey <anjany.pandey06@gmail.com>
2025-01-20 22:28:20 +0530

    `Added Auth0Provider to main.jsx for authentication`

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,18 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <Auth0Provider
+    domain="dev-uwmuixrpr6yb67gz.us.auth0.com"
+    clientId="0aLXpcYxGlMsZ7w4sk1GqDQKHJVWTngc"
+    authorizationParams={{
+      redirect_uri: "http://localhost:5173",
+    }}
+    audience="http://localhost:4000"
+    scope="openid profile email"
+  >
+    <App />
+  </Auth0Provider>
+);
