356d56edf99b8dd195f7074ea98f2a6b23110eb4
Anjany Pandey <anjany.pandey06@gmail.com>
2025-01-22 21:10:26 +0530

    `Added getAllFav API call and import to useFavourites hook`

--- frontend/src/hooks/useFavourites.jsx
+++ frontend/src/hooks/useFavourites.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useRef } from "react";
 import UserDetailContext from "../context/UserDetailContext";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useQuery } from "react-query";
+import { getAllFav } from "../utils/api";
 
 const useFavourites = () => {
   const { userDetails, setUserDetails } = useContext(UserDetailContext);
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -112,3 +112,25 @@ export const toFav = async (id, email, token) => {
     throw error;
   }
 };
+export const getAllFav = async (email, token) => {
+  if (!token) return;
+  try {
+    const res = await api.post(
+      `user/allFav/`,
+      {
+        email,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    console.log(res);
+    return res.data["favResidenciesID"]
+  } catch (error) {
+    toast.error("Something went wrong while fetching your fav list");
+    throw error;
+  }
+};
