1dd5f421f99ee9a0a923225912782f8408710a79
Anjany Pandey <anjany.pandey06@gmail.com>
2025-01-24 18:59:31 +0530

    `Added stepper component to AddPropertyModel component with multiple steps and state management for property details`

--- frontend/src/components/AddPropertyModel.jsx
+++ frontend/src/components/AddPropertyModel.jsx
@@ -1,7 +1,22 @@
-import { Modal } from "@mantine/core";
-import React from "react";
+import { Container, Modal, Stepper } from "@mantine/core";
+import React, { useState } from "react";
 
 const AddPropertyModel = ({ opened, setOpened }) => {
+  const [active, setActive] = useState(0);
+  const [propertyDetails, setPropertyDetails] = useState({
+    title: "",
+    description: "",
+    price: 0,
+    country: "",
+    city: "",
+    address: "",
+    image: null,
+    facilities: {
+      bedrooms: 0,
+      bathrooms: 0,
+      parking: 0,
+    },
+  });
   return (
     <Modal
       opened={opened}
@@ -9,7 +24,28 @@ const AddPropertyModel = ({ opened, setOpened }) => {
       closeOnClickOutside
       size={"90rem"}
     >
-      Add property Model
+      <Container h={"34rem"} w={"100%"}>
+        <>
+          <Stepper active={active} onStepClick={setActive}>
+            <Stepper.Step label="First step" description="Create an account">
+              <AddLocation
+                nextStep={nextStep}
+                propertyDetails={propertyDetails}
+                setPropertyDetails={setPropertyDetails}
+              />
+            </Stepper.Step>
+            <Stepper.Step label="Second step" description="Verify email">
+              Step 2 content: Verify email
+            </Stepper.Step>
+            <Stepper.Step label="Final step" description="Get full access">
+              Step 3 content: Get full access
+            </Stepper.Step>
+            <Stepper.Completed>
+              Completed, click back button to get to previous step
+            </Stepper.Completed>
+          </Stepper>
+        </>
+      </Container>
     </Modal>
   );
 };
