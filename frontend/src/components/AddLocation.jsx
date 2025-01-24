a69b7a20c5ffa417789ceeccef09db5e72734236
Anjany Pandey <anjany.pandey06@gmail.com>
2025-01-24 19:12:56 +0530

    `feat: updated AddLocation component with form functionality and props`

--- frontend/src/components/AddLocation.jsx
+++ frontend/src/components/AddLocation.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import {} from "@mantine/form";
 
-const AddLocation = () => {
-  return (
-    <div>AddLocation</div>
-  )
-}
+const AddLocation = ({ propertyDetails, setPropertyDetails, nextStep }) => {
+  const form = useForm({
+    initialValues: {
+      country: propertyDetails?.country,
+      city: propertyDetails?.city,
+      address: propertyDetails?.address,
+    },
+    validate: {
+      country: (value) => validateString(value),
+      city: (value) => validateString(value),
+      address: (value) => validateString(value),
+    },
+  });
 
-export default AddLocation
+  const { country, city, address } = form.values;
+  
+  return <div>AddLocation</div>;
+};
+
+export default AddLocation;
