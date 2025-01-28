c5c301235def8debb8a1c8a756fb10db23701c7e
Anjany Pandey <anjany.pandey06@gmail.com>
2025-01-28 13:51:08 +0530

    fix

--- backend/config/prismaConfig.js
+++ backend/config/prismaConfig.js
@@ -1,4 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  url: process.env.DATABASE_URL,
+});
 
-export {prisma}
+export { prisma };
