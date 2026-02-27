80dddaaf9b58e391e0de649682a6ea31904f0b9a
Anjany Pandey <anjany.pandey06@gmail.com>
2026-02-27 22:23:05 +0530

    testing prr

---
+++ frontend/src/pages/Test.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Test = () => {
+  return (
+    <div>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti sunt
+      sapiente velit voluptatem, deserunt veritatis mollitia possimus,
+      repellendus aspernatur accusamus alias ipsa provident debitis inventore,
+      neque optio nesciunt in expedita.
+    </div>
+  );
+};
+
+export default Test;
